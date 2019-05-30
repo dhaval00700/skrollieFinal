72005f4f5e511aa4ef135eb3b96732cb562f3023
binalnasit26 <binal.nasit1@gmail.com>
2019-05-30 18:06:25 +0530

    commnet add

--- SKROLLIE/SKROLLIE/ViewControllerFileas/Profile/CustomCells/AllCommentsTableViewCell.swift
+++ SKROLLIE/SKROLLIE/ViewControllerFileas/Profile/CustomCells/AllCommentsTableViewCell.swift
@@ -8,26 +8,61 @@
 
 import UIKit
 
-class AllCommentsTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var imgUser: UIImageView!
+protocol delegateSelectOfComment {
+}
+
+class AllCommentsTableViewCell: UITableViewCell,UITableViewDelegate,UITableViewDataSource {
     
+    @IBOutlet weak var imgUser: UIImageView!    
     @IBOutlet weak var lblUser: UILabel!
     @IBOutlet weak var lblUserComment: UILabel!
-    
     @IBOutlet weak var lctSubCommentTableHeight: NSLayoutConstraint!
     @IBOutlet weak var tblSubComment: UITableView!
+    
+    var aryUserList = [String]()
+    var aryImg = [String]()
+    var imgOfUser = String()
+    var Username = String()
+    var delegateOfSelectedComment : delegateSelectOfComment!
 
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
+        
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        tblSubComment.delegate = self
+        tblSubComment.dataSource = self
+        
+        tblSubComment.register(UINib(nibName: "CommentItemTableViewCell", bundle: nil), forCellReuseIdentifier: "CommentItemTableViewCell")
+        
+        aryUserList = ["@horedude","@doggylover","@happyCampper"]
+        aryImg = ["img6","img6","img6"]
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return aryImg.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+         imgOfUser = aryImg[indexPath.row]
+         Username = aryUserList[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CommentItemTableViewCell", for: indexPath) as! CommentItemTableViewCell
+        cell.imgUser.image = UIImage.init(named: imgOfUser)
+        cell.lblUser.text = Username
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+            print("hello")
+        let strSelect = aryImg[indexPath.row]
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool){
+        super.setSelected(selected, animated: animated)
+    }
 }
